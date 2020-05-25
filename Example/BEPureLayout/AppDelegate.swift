883f6b680e7a12aeed6b5497e734aee8a93d6663
Chung Tran <bigearsenal@gmail.com>
2020-05-25 11:05:02 +0700

    Remove storyboard. Add demo for UIView's methods

--- Example/BEPureLayout/AppDelegate.swift
+++ Example/BEPureLayout/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Get rid of storyboards
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        
         return true
     }
 
--- Example/BEPureLayout/ViewController.swift
+++ Example/BEPureLayout/ViewController.swift
@@ -7,17 +7,34 @@
 //
 
 import UIKit
+import BEPureLayout
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setUp()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setUp() {
+        let topLeftView = UIView(width: 100, height: 100, backgroundColor: .green, cornerRadius: 16)
+        view.addSubview(topLeftView)
+        topLeftView.autoPinToTopLeftCornerOfSuperviewSafeArea(xInset: 16, yInset: 16)
+        
+        let topRightView = UIView(width: 100, height: 100, backgroundColor: .red, cornerRadius: 16)
+        view.addSubview(topRightView)
+        topRightView.autoPinToTopRightCornerOfSuperviewSafeArea(xInset: 16)
+        
+        let bottomLeftView = UIView(width: 100, height: 100, backgroundColor: .blue, cornerRadius: 16)
+        view.addSubview(bottomLeftView)
+        bottomLeftView.autoPinToBottomLeftCornerOfSuperview(xInset: 16)
+        
+        let bottomRightView = UIView(width: 100, height: 100, backgroundColor: .yellow, cornerRadius: 16)
+        view.addSubview(bottomRightView)
+        bottomRightView.autoPinToBottomRightCornerOfSuperview(xInset: 16)
     }
 
 }
