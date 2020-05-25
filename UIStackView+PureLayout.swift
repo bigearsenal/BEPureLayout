2ba8c1ccff9dcbc62181f235428ee73447ecf655
Chung Tran <bigearsenal@gmail.com>
2020-05-25 16:00:42 +0700

    add UIImageView, UITextField, UITextView

--- UIStackView+PureLayout.swift
+++ UIStackView+PureLayout.swift
@@ -9,8 +9,12 @@ import Foundation
 
 @available(iOS 9.0, *)
 public extension UIStackView {
-    convenience init(axis: NSLayoutConstraint.Axis, spacing: CGFloat? = nil, alignment: UIStackView.Alignment = .center, distribution: UIStackView.Distribution = .fill)
-    {
+    convenience init(
+        axis: NSLayoutConstraint.Axis,
+        spacing: CGFloat? = nil,
+        alignment: UIStackView.Alignment = .center,
+        distribution: UIStackView.Distribution = .fill
+    ) {
         self.init(forAutoLayout: ())
         self.axis = axis
         self.alignment = alignment
