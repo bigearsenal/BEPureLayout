82892437c4eaf17e523f1ac8c57772e41d6cd358
Chung Tran <bigearsenal@gmail.com>
2020-05-26 09:08:54 +0700

    Add ContentHuggingScrollView

---
+++ ContentHuggingScrollView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentHuggingScrollView.swift
+//  Commun
+//
+//  Created by Chung Tran on 11/1/19.
+//  Copyright © 2019 Commun Limited. All rights reserved.
+//
+
+import Foundation
+
+@available(iOS 9.0, *)
+public class ContentHuggingScrollView: UIScrollView {
+    // MARK: - Subviews
+    lazy var contentView = UIView(forAutoLayout: ())
+    var scrollableAxis: NSLayoutConstraint.Axis
+    
+    // MARK: - Methods
+    init(scrollableAxis: NSLayoutConstraint.Axis, contentInset: UIEdgeInsets = .zero) {
+        self.scrollableAxis = scrollableAxis
+        super.init(frame: .zero)
+        self.contentInset = contentInset
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func commonInit() {
+        configureForAutoLayout()
+        addSubview(contentView)
+        contentView.autoPinEdgesToSuperviewEdges()
+        if scrollableAxis == .vertical {
+            contentView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+        } else {
+            contentView.heightAnchor.constraint(equalTo: heightAnchor).isActive = true
+        }
+    }
+}
