c4531861b75886f97312e60f0d62a5bef8d471bb
Chung Tran <bigearsenal@gmail.com>
2020-05-31 10:48:45 +0700

    add BENavigationController + BEViewController

---
+++ BENavigationController.swift
@@ -0,0 +1,36 @@
+//
+//  BENavigationController.swift
+//  BEPureLayout
+//
+//  Created by Chung Tran on 5/31/20.
+//
+
+import Foundation
+
+public class BENavigationController: UINavigationController {
+    // MARK: - Properties
+    private var statusBarStyle: UIStatusBarStyle = .default
+    public override var preferredStatusBarStyle: UIStatusBarStyle {self.statusBarStyle}
+    
+    public var previousController: UIViewController? {
+        if viewControllers.count > 1 {
+            return viewControllers[viewControllers.count-2]
+        }
+        return nil
+    }
+    
+    // MARK: - Custom methods
+    public func changeStatusBarStyle(_ style: UIStatusBarStyle) {
+        self.statusBarStyle = style
+        setNeedsStatusBarAppearanceUpdate()
+    }
+    
+    public override func popViewController(animated: Bool) -> UIViewController? {
+        let vc = super.popViewController(animated: animated)
+        if let vc = previousController as? BEViewController {
+            vc.configureNavigationBar()
+            vc.changeStatusBarStyle(vc.preferredStatusBarStyle)
+        }
+        return vc
+    }
+}
