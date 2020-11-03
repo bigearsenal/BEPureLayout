60e0a1f52f3fe2b573938ed82d3ca04a01e52c18
Chung Tran <bigearsenal@gmail.com>
2020-11-03 12:05:27 +0700

    add UIEdgeInsets Extensions

---
+++ BEPureLayout/Classes/UIEdgeInsets+Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  UIEdgeInsets+Extensions.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 11/3/20.
+//
+
+import Foundation
+
+public extension UIEdgeInsets {
+    func modifying(dTop: CGFloat? = nil, dLeft: CGFloat? = nil, dBottom: CGFloat? = nil, dRight: CGFloat? = nil) -> UIEdgeInsets {
+        var insets = self
+        if let dTop = dTop {
+            insets.top += dTop
+        }
+        if let dLeading = dLeft {
+            insets.left += dLeading
+        }
+        if let dBottom = dBottom {
+            insets.bottom += dBottom
+        }
+        if let dRight = dRight {
+            insets.right += dRight
+        }
+        return insets
+    }
+    
+    init(x: CGFloat, y: CGFloat) {
+        self.init(top: y, left: x, bottom: y, right: x)
+    }
+    
+    init(all inset: CGFloat) {
+        self.init(top: inset, left: inset, bottom: inset, right: inset)
+    }
+}
