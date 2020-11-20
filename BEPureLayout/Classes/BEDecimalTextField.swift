4f52975ced50b7b4f32bfd60d3e6f9495bc24276
Chung Tran <bigearsenal@gmail.com>
2020-11-20 15:06:49 +0700

    add BEDecimalTextField

---
+++ BEPureLayout/Classes/BEDecimalTextField.swift
@@ -0,0 +1,63 @@
+//
+//  BEDecimalTextField.swift
+//  BEPureLayout
+//
+//  Created by Chung Tran on 20/11/2020.
+//
+
+import Foundation
+
+open class BEDecimalTextField: UITextField {
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    public func commonInit() {
+        
+    }
+    
+    /// Use this function for validating textfield in UITextFieldDelegate.
+    ///
+    /// ```
+    /// func textField(_ textField: UITextField, shouldChangeCharactersIn range...
+    ///     if (textField as? DecimalTextField)?.shouldChangeCharactersInRange(range, replacementString: string) == true {...}
+    ///     ...
+    /// }
+    /// ```
+    open func shouldChangeCharactersInRange(_ range: NSRange, replacementString string: String) -> Bool {
+        // if input comma (or dot)
+        if text?.isEmpty == true, string == Locale.current.decimalSeparator {
+            text = "0\(Locale.current.decimalSeparator ?? ".")"
+            return false
+        }
+        
+        // if deleting
+        if string.isEmpty { return true }
+        
+        // get the current text, or use an empty string if that failed
+        let currentText = text ?? ""
+
+        // attempt to read the range they are trying to change, or exit if we can't
+        guard let stringRange = Range(range, in: currentText) else { return false }
+
+        // add their new text to the existing text
+        var updatedText = currentText.replacingCharacters(in: stringRange, with: string)
+        
+        // check if newText is a Number
+        let formatter = NumberFormatter()
+        let isANumber = formatter.number(from: updatedText) != nil
+        
+        if updatedText.starts(with: "0") && !updatedText.starts(with: "0\(Locale.current.decimalSeparator ?? ".")") {
+            updatedText = currentText.replacingOccurrences(of: "^0+", with: "", options: .regularExpression)
+            text = updatedText
+        }
+        
+        return isANumber
+    }
+}
