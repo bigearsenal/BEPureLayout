c39acee4acfc43f17cd11cd8425d5fe1e125d986
Chung Tran <bigearsenal@gmail.com>
2021-01-18 09:29:53 +0700

    edit file scope

--- BEPureLayout/Classes/UIViewControllerPreview.swift
+++ BEPureLayout/Classes/UIViewControllerPreview.swift
@@ -9,14 +9,14 @@ import Foundation
 import SwiftUI
 
 @available(iOS 13, *)
-struct UIViewControllerPreview<ViewController: UIViewController>: UIViewControllerRepresentable {
+public struct UIViewControllerPreview<ViewController: UIViewController>: UIViewControllerRepresentable {
     let viewController: ViewController
 
-    init(_ builder: @escaping () -> ViewController) {
+    public init(_ builder: @escaping () -> ViewController) {
         viewController = builder()
     }
 
-    func makeUIViewController(context: Context) -> ViewController { viewController }
+    public func makeUIViewController(context: Context) -> ViewController { viewController }
 
-    func updateUIViewController(_ uiViewController: ViewController, context: Context) {}
+    public func updateUIViewController(_ uiViewController: ViewController, context: Context) {}
 }
