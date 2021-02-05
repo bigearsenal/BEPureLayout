eb7690aa3f460782fffb7e730148c34d03bf87a0
Chung Tran <bigearsenal@gmail.com>
2021-02-05 23:56:23 +0700

    disable required init(coder:)

--- BEPureLayout/Classes/BEViewController.swift
+++ BEPureLayout/Classes/BEViewController.swift
@@ -19,6 +19,25 @@ open class BEViewController: UIViewController {
     open var preferredBackgroundColor: UIColor {.white}
     open var preferredNavigationBarStype: NavigationBarStyle {.normal()}
     
+    // MARK: - Initializers
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable,
+    message: "Loading this view controller from a nib is unsupported in favor of initializer dependency injection."
+    )
+    public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    @available(*, unavailable,
+    message: "Loading this view controller from a nib is unsupported in favor of initializer dependency injection."
+    )
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("Loading this view controller from a nib is unsupported in favor of initializer dependency injection.")
+    }
+    
     // MARK: - Lifecycle methods
     open override func viewDidLoad() {
         super.viewDidLoad()
