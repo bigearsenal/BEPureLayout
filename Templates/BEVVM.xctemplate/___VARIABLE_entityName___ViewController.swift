039dc0b47d693e54fffc8d257bbceee83ba44ef9
Chung Tran <bigearsenal@gmail.com>
2021-02-07 00:28:59 +0700

    add BEVVM XCode template

---
+++ Templates/BEVVM.xctemplate/___VARIABLE_entityName___ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+class ___VARIABLE_entityName___ViewController: ___VARIABLE_baseClass___ {
+    
+    // MARK: - Properties
+//    let viewModel: ___VARIABLE_entityName___ViewModel
+    
+    // MARK: - Subviews
+    
+    // MARK: - Methods
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    override func bind() {
+        super.bind()
+        
+    }
+    
+    // MARK: - Helpers
+}
+
+@available(iOS 13, *)
+struct ___VARIABLE_entityName___ViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            UIViewControllerPreview {
+                ___VARIABLE_entityName___ViewController()
+            }
+            .previewDevice("iPhone SE (2nd generation)")
+        }
+    }
+}
---
+++ Templates/BEVVM.xctemplate/___VARIABLE_entityName___ViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+
+struct ___VARIABLE_entityName___ViewModel {
+    // MARK: - Constants
+    
+    // MARK: - Properties
+    let bag = DisposeBag()
+    
+    // MARK: - Subjects
+    
+}
