ab9d63b042762e37148c510520d0b4fbe81ddcc6
Chung Tran <bigearsenal@gmail.com>
2021-03-12 15:33:38 +0700

    fix init(all:)

--- BEPureLayout/Classes/UIEdgeInsets+Extensions.swift
+++ BEPureLayout/Classes/UIEdgeInsets+Extensions.swift
@@ -33,8 +33,22 @@ public extension UIEdgeInsets {
         self.init(top: y, left: x, bottom: y, right: x)
     }
     
-    init(all inset: CGFloat) {
+    init(all inset: CGFloat, excludingEdge: UIRectEdge? = nil) {
         self.init(top: inset, left: inset, bottom: inset, right: inset)
+        if let excludingEdge = excludingEdge {
+            switch excludingEdge {
+            case .top:
+                self.modify(dTop: -inset)
+            case .left:
+                self.modify(dLeft: -inset)
+            case .bottom:
+                self.modify(dBottom: -inset)
+            case .right:
+                self.modify(dRight: -inset)
+            default:
+                break
+            }
+        }
     }
     
     init(only edge: UIRectEdge, inset: CGFloat) {
