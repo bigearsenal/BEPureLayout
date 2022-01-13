2db132ba8fc597db45848ab4ab6440ca93fdcef1
Tran Giang Long <longtran@Mac-mini-Tran.local>
2022-01-13 12:00:24 +0300

    feat(zstack): improve

--- Sources/BEPureLayout/BEZStack.swift
+++ Sources/BEPureLayout/BEZStack.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import PureLayout
 
 open class BEZStack: BEView {
     let children: [UIView]
@@ -29,5 +30,37 @@ open class BEZStack: BEView {
     open override func addSubview(_ view: UIView) {
         fatalError("addSubview is not allow!")
     }
+}
 
+open class BEZStackPosition: BEView {
+    
+    public enum Mode {
+        case fill
+        case center
+    }
+    
+    let mode: Mode
+    let child: UIView
+    
+    required public init(mode: Mode = .fill, @BEViewBuilder builder: Builder) {
+        self.mode = mode
+        child = builder().build()
+        super.init(frame: .zero)
+    }
+    
+    final public override func commonInit() {
+        super.commonInit()
+        addSubview(child)
+    }
+    
+    open override func didMoveToSuperview() {
+        switch (mode) {
+        case .center:
+            for constraint in child.autoCenterInSuperview() { constraint.isActive = true }
+        case .fill:
+            child.autoPinEdgesToSuperviewEdges()
+        }
+        
+        autoPinEdgesToSuperviewEdges()
+    }
 }
