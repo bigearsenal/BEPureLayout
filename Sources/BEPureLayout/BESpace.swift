d7aab6ccf283913ca772c30f59733779fea0aee7
Giang Long Tran <trangianglong4003@gmail.com>
2022-06-15 17:32:41 +0200

    feat(spacer)

---
+++ Sources/BEPureLayout/BESpace.swift
@@ -0,0 +1,27 @@
+// Copyright 2022 P2P Validator Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+import Foundation
+
+/// Take all available space in UIStackView
+class BESpace: UIView {
+    enum Type {
+        case horizontal
+        case vertical
+    }
+    
+    let type: Type
+    
+    override init(type: Type) {
+        self.type = type
+        super.init(frame: .zero)
+    }
+    
+    override var intrinsicContentSize: CoreGraphics.CGSize {
+        switch type {
+        case .horizontal: return .init(width: CGFloat.infinity, height: 0)
+        case .vertical: return .init(width: 0, height: CGFloat.infinity)
+        }
+    }
+}
